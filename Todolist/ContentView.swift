2c6672df8031efad05af0f7c404f9c762ce01602
Shao-Ting Huang <shaotinghuang.tim@gmail.com>
2020-06-26 11:25:18 -0700

    Initial Commit

---
+++ Todolist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todolist
+//
+//  Created by Shao Ting Huang on 6/26/20.
+//  Copyright © 2020 Shao Ting Huang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
